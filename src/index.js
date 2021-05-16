fe2c580dc684c58c8a720ec43e3b116d4efaee71
Santhoshkumar <skrece22@gmail.com>
2021-05-16 18:47:36 +0530

    Load CSS with webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import './style.css'
 
 const Greeting = () => <h1>Hello from React</h1>
 
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,14 @@ module.exports = {
           presets: ['@babel/preset-env', '@babel/preset-react']
         }
       } 
-    }]
+    },
+    {
+      test: /\.css$/,
+      use: [
+        {loader: 'style-loader'},
+        {loader: 'css-loader'}
+      ]
+    }
+  ]
   }
 }  
