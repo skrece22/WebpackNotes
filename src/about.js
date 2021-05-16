1a72df6c38c8e1de0d850134089e917f6eb534b9
Santhoshkumar <skrece22@gmail.com>
2021-05-16 19:15:55 +0530

    Code splitting - adde multiple entry points

---
+++ src/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Webpack</h1>
+    </div>
+  )
+}
+
+render(
+  <About/>,
+  document.getElementById('target')
+)
